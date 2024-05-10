e6ffa9b5b08b4f314b1d3bb8310103e1005ac8af
geordangesink <georgegeordan@gmail.com>
2024-05-10 02:47:46 +0200

    NASA picture browser starting page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+document.querySelector("#options").addEventListener("click", toggleOptions)
+
 document.querySelector("#form").addEventListener("keypress", function(event)
 {
     if (event.key === "Enter")
@@ -7,17 +9,21 @@ document.querySelector("#form").addEventListener("keypress", function(event)
     }
 });
 
-document.querySelector("#request").addEventListener("click", function(event)
-{
-    event.preventDefault();
+document.querySelector("#date").oninput = function(){
     getFetch();
-});
+}
+
+// document.querySelector("#request").addEventListener("click", function(event)
+// {
+//     event.preventDefault();
+//     getFetch();
+// });
 
 function getFetch()
 {
     document.querySelector("img").src = "";
     document.querySelector("iframe").src = "";
-    const choice = document.querySelector("#search-query").value.toLowerCase();
+    const choice = document.querySelector("#date").value.toLowerCase();
     const url = `https://api.nasa.gov/planetary/apod?api_key=ObdEBTvaVIHVkPcwQ2DgSiCvneYh6t0QNZu2gTNd&date=${choice}`;
     const options = {
         method : "GET"
@@ -28,20 +34,26 @@ function getFetch()
         .then(data => {
             if (data.media_type == "image")
             {
+                document.querySelector("iframe").classList.add("hide");
+                document.querySelector("img").classList.remove("hide");
                 document.querySelector("img").src = data.hdurl;
             }
             else
             {
+                document.querySelector("img").classList.add("hide");
+                document.querySelector("iframe").classList.remove("hide");
                 document.querySelector("iframe").src = data.url;
             }
-
-            document.querySelector("h3").innerText = "Explanation: ";
             document.querySelector("p").innerText = data.explanation;
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
 }
+getFetch();
 
-
-
+function toggleOptions(){
+    document.querySelectorAll(".show").forEach(element =>{
+        element.classList.toggle("hide");
+    });
+}
